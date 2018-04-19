ea4eff611063528b61903eb614875c6bd2ad6544
Gregory Schmitt <gjschmitt@live.com>
2018-04-19 19:09:54 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+function theBeatlesPlay(musicians, instruments) {
+  var emptyArray = [];
+  var i;
+  
+  for (i=0; i < musicians.length; i++) {
+    emptyArray.push(musicians[i] + " plays " + instruments[i])
+  }
+  return(emptyArray)
+}
+
+function johnLennonFacts(factsOfLennon) {
+  var emptyArray = [];
+  var i = 0;
+  
+  while (i < factsOfLennon.length) {
+    emptyArray.push(factsOfLennon[i] + "!")
+    i++;
+  }
+    return(emptyArray)
+}
